34d4434c085ec2ca0e248d5b45e1f0e33aa3b35c
Alexei Diaz <alexei.eleusis@gmail.com>
2026-05-30 16:51:46 -0700

    feat: add rule no-keyof-any

---
+++ eslint-lensflow-plugin/tests/rules/no-keyof-any.test.ts
@@ -0,0 +1,24 @@
+import { ruleTester } from "../helpers/rule-tester.js";
+import rule from "../../src/rules/no-keyof-any.js";
+
+ruleTester.run("no-keyof-any", rule, {
+  valid: [
+    `function get<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}`,
+    `type Keys = keyof string;`,
+    `type Props = keyof HTMLDivElement;`,
+  ],
+  invalid: [
+    {
+      code: `type Keys = keyof any;`,
+      errors: [{ messageId: "keyofAny" }],
+    },
+    {
+      code: `function get<T, K extends keyof any>(obj: T, key: K) {
+  return obj[key];
+}`,
+      errors: [{ messageId: "keyofAny" }],
+    },
+  ],
+});
