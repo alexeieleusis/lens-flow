3d049ed0ddb62cac2b7406a03f3a0684018ae594
Alexei Diaz <alexei.eleusis@gmail.com>
2026-05-30 13:30:41 -0700

    feat: add rule no-assertnever-cast-unknown

---
+++ eslint-lensflow-plugin/src/rules/no-assertnever-cast-unknown.ts
@@ -0,0 +1,41 @@
+import { createRule } from "../utils/rule-creator.js";
+import type { TSESLint } from "@typescript-eslint/utils";
+
+export default createRule({
+  name: "no-assertnever-cast-unknown",
+  meta: {
+    type: "problem",
+    docs: {
+      description:
+        "Disallow casting to unknown before passing to assertNever/assertExhaustive, which bypasses exhaustiveness checking",
+    },
+    messages: {
+      bypassExhaustiveness:
+        "Casting to unknown before assertNever/assertExhaustive bypasses the exhaustiveness check. Remove the `as unknown` cast. See: https://raw.githubusercontent.com/jpablo/vibe-types/refs/heads/main/plugin/skills/typescript/catalog/T34-never-bottom.md",
+    },
+    schema: [],
+    fixable: undefined,
+  },
+  defaultOptions: [],
+  create(context: TSESLint.RuleContext<"bypassExhaustiveness", []>) {
+    return {
+      CallExpression(node) {
+        if (node.callee.type !== "Identifier") return;
+        if (!/^assertNever$/i.test(node.callee.name) && !/^assertExhaustive$/i.test(node.callee.name)) {
+          return;
+        }
+        for (const arg of node.arguments) {
+          if (
+            arg.type === "TSAsExpression" &&
+            arg.typeAnnotation.type === "TSUnknownKeyword"
+          ) {
+            context.report({
+              node: arg,
+              messageId: "bypassExhaustiveness",
+            });
+          }
+        }
+      },
+    };
+  },
+});
