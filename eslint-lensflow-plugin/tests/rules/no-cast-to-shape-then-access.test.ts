4acf06f0c61d7d0aa6d95dbdf1c9b03c90a4b29b
Alexei Diaz <alexei.eleusis@gmail.com>
2026-05-30 13:34:43 -0700

    feat: add rule no-cast-to-shape-then-access

---
+++ eslint-lensflow-plugin/tests/rules/no-cast-to-shape-then-access.test.ts
@@ -0,0 +1,26 @@
+import { ruleTester } from "../helpers/rule-tester.js";
+import rule from "../../src/rules/no-cast-to-shape-then-access.js";
+
+ruleTester.run("no-cast-to-shape-then-access", rule, {
+  valid: [
+    `function getId<T extends { id: string }>(obj: { data: T }): string {
+  return obj.data.id;
+}`,
+    `const x = obj.data as { id: string };`,
+    `const x = (obj.data as string).length;`,
+  ],
+  invalid: [
+    {
+      code: `function getId(obj: { data: any }) {
+  return (obj.data as { id: string }).id;
+}`,
+      errors: [{ messageId: "castToShapeThenAccess" }],
+    },
+    {
+      code: `function getName(obj: { data: any }) {
+  return (obj.data as { name: string; age: number }).name;
+}`,
+      errors: [{ messageId: "castToShapeThenAccess" }],
+    },
+  ],
+});
