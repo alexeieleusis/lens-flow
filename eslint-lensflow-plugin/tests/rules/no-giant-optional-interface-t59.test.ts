5696a42716528871e72187fc93e59e5c7d094e44
Alexei Diaz <alexei.eleusis@gmail.com>
2026-05-30 16:43:10 -0700

    feat: add rule no-giant-optional-interface-t59

---
+++ eslint-lensflow-plugin/tests/rules/no-giant-optional-interface-t59.test.ts
@@ -0,0 +1,76 @@
+import { ruleTester } from "../helpers/rule-tester.js";
+import rule from "../../src/rules/no-giant-optional-interface-t59.js";
+
+ruleTester.run("no-giant-optional-interface-t59", rule, {
+  valid: [
+    `interface SmallConfig {
+      text?: string;
+      icon?: string;
+      onClick?: () => void;
+    }`,
+    `interface MixedProps {
+      requiredField: string;
+      optionalField?: number;
+      anotherRequired: boolean;
+    }`,
+    `interface JustUnderLimit {
+      a?: string;
+      b?: string;
+      c?: string;
+      d?: string;
+      e?: string;
+      f?: string;
+      g?: string;
+      h?: string;
+      i?: string;
+    }`,
+  ],
+  invalid: [
+    {
+      code: `interface ButtonConfig {
+        text?: string;
+        icon?: string;
+        onClick?: () => void;
+        onHover?: () => void;
+        disabled?: boolean;
+        loading?: boolean;
+        ariaLabel?: string;
+        variant?: string;
+        size?: string;
+        color?: string;
+      }`,
+      errors: [{ messageId: "tooManyOptional" }],
+    },
+    {
+      code: `interface GiantConfig {
+        a?: string;
+        b?: string;
+        c?: string;
+        d?: string;
+        e?: string;
+        f?: string;
+        g?: string;
+        h?: string;
+        i?: string;
+        j?: string;
+        k?: string;
+      }`,
+      errors: [{ messageId: "tooManyOptional" }],
+    },
+    {
+      code: `interface ExactlyAtLimit {
+        a?: string;
+        b?: string;
+        c?: string;
+        d?: string;
+        e?: string;
+        f?: string;
+        g?: string;
+        h?: string;
+        i?: string;
+        j?: string;
+      }`,
+      errors: [{ messageId: "tooManyOptional" }],
+    },
+  ],
+});
