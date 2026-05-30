693361529372b7b0cddfc93bf1ab797fae55832a
Alexei Diaz <alexei.eleusis@gmail.com>
2026-05-30 13:30:57 -0700

    feat: add rule no-bivariant-method-syntax

---
+++ eslint-lensflow-plugin/src/rules/no-bivariant-method-syntax.ts
@@ -0,0 +1,24 @@
+import { createRule } from "../utils/rule-creator.js";
+import type { TSESLint } from "@typescript-eslint/utils";
+import { createBivariantMethodVisitor } from "../utils/bivariant-method-checker.js";
+
+export default createRule({
+  name: "no-bivariant-method-syntax",
+  meta: {
+    type: "problem",
+    docs: {
+      description:
+        "Disallow method-signature syntax in interfaces and type literals, which causes bivariant (unsound) parameter checking",
+    },
+    messages: {
+      methodSyntax:
+        "Method signature '{{name}}' uses bivariant syntax. Use function-property syntax (e.g. '{{name}}: ({{params}}) => {{returnType}}') for contravariant parameter checking. See: https://raw.githubusercontent.com/jpablo/vibe-types/refs/heads/main/plugin/skills/typescript/catalog/T08-variance-subtyping.md",
+    },
+    schema: [],
+    fixable: undefined,
+  },
+  defaultOptions: [],
+  create(context: TSESLint.RuleContext<"methodSyntax", []>) {
+    return createBivariantMethodVisitor(context);
+  },
+});
