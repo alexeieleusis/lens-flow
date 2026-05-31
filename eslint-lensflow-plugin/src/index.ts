2e0ef602a2aedafdf95fc72fad1296599131d6bd
Alexei Diaz <alexei.eleusis@gmail.com>
2026-05-31 15:07:47 -0700

    review: address copilot comments

--- eslint-lensflow-plugin/src/index.ts
+++ eslint-lensflow-plugin/src/index.ts
@@ -1,10 +1,13 @@
 import type { TSESLint } from "@typescript-eslint/utils";
+import noAnyExternalData from "./rules/no-any-external-data.js";
 
 const plugin: {
   rules: Record<string, TSESLint.RuleModule<string, unknown[]>>;
   configs: Record<string, unknown>;
 } = {
-  rules: {},
+  rules: {
+    "no-any-external-data": noAnyExternalData,
+  },
   configs: {},
 };
 
