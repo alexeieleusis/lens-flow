e426f6f610853e66396a35773d6291f0ed5af552
Alexei Diaz <alexei.eleusis@gmail.com>
2026-05-30 20:46:45 -0700

    feat: add rule require-typestate-rebinding

---
+++ eslint-lensflow-plugin/tests/rules/require-typestate-rebinding.test.ts
@@ -0,0 +1,74 @@
+import { ruleTester } from "../helpers/rule-tester.js";
+import rule from "../../src/rules/require-typestate-rebinding.js";
+
+ruleTester.run("require-typestate-rebinding", rule, {
+  valid: [
+    // Correct: rebind to the original let variable
+    `let conn = Db.open();
+conn = conn.connect();
+conn.query("SELECT 1");`,
+
+    // No let binding involved
+    `const conn = Db.open();
+const advanced = conn.connect();`,
+
+    // let variable not used after the const assignment
+    `let conn = Db.open();
+const advanced = conn.connect();
+advanced.query("SELECT 1");`,
+
+    // const assignment but not a method call on let variable
+    `let conn = Db.open();
+const timeout = 5000;
+conn.query("SELECT 1");`,
+
+    // Method call result assigned to let variable (correct pattern)
+    `let conn = Db.open();
+let advanced = conn.connect();
+advanced.query("SELECT 1");`,
+
+    // Multiple let variables, no stale reference
+    `let a = createA();
+let b = a.transform();
+b.use();`,
+  ],
+  invalid: [
+    // Basic antipattern: const assignment of method call on let, then let used
+    {
+      code: `let conn = Db.open();
+const advanced = conn.connect();
+conn.query("SELECT 1");`,
+      errors: [{ messageId: "staleStateRef" }],
+    },
+    // Multiple method calls in chain
+    {
+      code: `let client = Api.create();
+const authenticated = client.authenticate();
+client.request("/data");`,
+      errors: [{ messageId: "staleStateRef" }],
+    },
+    // Used in a different expression context
+    {
+      code: `let db = Database.connect();
+const transaction = db.beginTransaction();
+console.log(db);`,
+      errors: [{ messageId: "staleStateRef" }],
+    },
+    // Used in a conditional
+    {
+      code: `let state = Machine.init();
+const running = state.start();
+if (state.isReady) {
+  state.process();
+}`,
+      errors: [{ messageId: "staleStateRef" }],
+    },
+    // Optional chain call on let variable
+    {
+      code: `let conn = Db.open();
+const advanced = conn?.connect();
+conn.query("SELECT 1");`,
+      errors: [{ messageId: "staleStateRef" }],
+    },
+  ],
+});
