86d4c815bc8e511fa651f013fe23c5fc9cdf52be
Alexei Diaz <alexei.eleusis@gmail.com>
2026-05-30 17:06:14 -0700

    feat: add rule no-mixed-decorator-apis

---
+++ eslint-lensflow-plugin/tests/rules/no-mixed-decorator-apis.test.ts
@@ -0,0 +1,49 @@
+import { ruleTester } from "../helpers/rule-tester.js";
+import rule from "../../src/rules/no-mixed-decorator-apis.js";
+
+ruleTester.run("no-mixed-decorator-apis", rule, {
+  valid: [
+    // Only stage-3 decorators
+    `function register(target: unknown, ctx: ClassDecoratorContext) {
+      ctx.addInitializer(function (this: unknown) {
+        console.log(String(ctx.name) + " constructed");
+      });
+    }
+
+    @register
+    class Foo {}`,
+
+    // Only experimental decorators
+    `function log(target: typeof MyClass) {}
+
+    @log
+    class MyClass {}`,
+
+    // Mixed definitions but only one pattern used on the class
+    `function stage3Deco(target: unknown, ctx: ClassDecoratorContext) {}
+    function expDeco(target: typeof A) {}
+
+    @stage3Deco
+    class A {}`,
+  ],
+  invalid: [
+    {
+      code: `function stage3Decorator(target: any, ctx: ClassDecoratorContext) {}
+function experimentalDecorator(target: typeof Foo) {}
+
+@experimentalDecorator
+@stage3Decorator
+class Foo {}`,
+      errors: [{ messageId: "mixedDecoratorApis" }],
+    },
+    {
+      code: `function inject(target: unknown, ctx: ClassMethodDecoratorContext) {}
+function freeze(target: typeof Service) {}
+
+@freeze
+@inject
+class Service {}`,
+      errors: [{ messageId: "mixedDecoratorApis" }],
+    },
+  ],
+});
