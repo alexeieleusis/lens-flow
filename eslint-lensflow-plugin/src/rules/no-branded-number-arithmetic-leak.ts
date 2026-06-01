5b47bdb83e584d061aa4129569e47869b0a11dd8
Alexei Diaz <alexei.eleusis@gmail.com>
2026-06-01 01:47:40 -0700

    review: address copilot comments

--- eslint-lensflow-plugin/src/rules/no-branded-number-arithmetic-leak.ts
+++ eslint-lensflow-plugin/src/rules/no-branded-number-arithmetic-leak.ts
@@ -11,9 +11,7 @@ function hasBrandProperty(type: ts.Type): boolean {
   const props = type.getProperties();
   return props.some((p) => {
     const name = p.escapedName as string;
-    // TypeScript escapes __ prefixed names by adding a leading _,
-    // so __brand's escapedName is ___brand. Use case-insensitive regex.
-    return /brand$/i.test(name);
+    return name === "_brand" || name === "__brand" || /Brand$/.test(name);
   });
 }
 
