e659409ef227af106446fdac84583094dbf48a78
Alexei Diaz <alexei.eleusis@gmail.com>
2026-06-01 01:47:40 -0700

    review: address copilot comments

--- eslint-lensflow-plugin/src/rules/no-branded-number-arithmetic-leak.ts
+++ eslint-lensflow-plugin/src/rules/no-branded-number-arithmetic-leak.ts
@@ -10,13 +10,15 @@ const ARITHMETIC_OPS = new Set(["+", "-", "*", "/", "%"]);
 function hasBrandProperty(type: ts.Type): boolean {
   const props = type.getProperties();
   return props.some((p) => {
-    const name = String(p.escapedName);
-    return /_*brand$/i.test(name);
+    const name = p.escapedName as string;
+    return name === "_brand" || name === "__brand" || /Brand$/.test(name);
   });
 }
 
 function isBrandedNumber(checker: ts.TypeChecker, tsType: ts.Type): boolean {
-  const constituents = (tsType as ts.IntersectionType)?.types;
+  const apparent = checker.getApparentType(tsType);
+
+  const constituents = (apparent as ts.IntersectionType)?.types;
   if (!constituents || constituents.length <= 1) return false;
 
   let hasNumber = false;
