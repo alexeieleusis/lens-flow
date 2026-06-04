c85ca092645001523f83eb53b54c4af3e1f47390
Alexei Diaz <alexei.eleusis@gmail.com>
2026-06-04 06:47:03 -0700

    fix: address build and test failures

--- eslint-lensflow-plugin/tests/rules/no-blind-as-cast.test.ts
+++ eslint-lensflow-plugin/tests/rules/no-blind-as-cast.test.ts
@@ -11,8 +11,8 @@ RuleTester.it = it;
 
 const __dirname = path.resolve(fileURLToPath(import.meta.url), "..");
 const TEST_FILENAME = "file.ts";
-const TS_CONFIG_DIR = path.resolve(__dirname, "../..");
-const TS_CONFIG = path.join(TS_CONFIG_DIR, "tsconfig.test.json");
+const TS_CONFIG_DIR = path.resolve(__dirname, "..");
+const TS_CONFIG = path.join(TS_CONFIG_DIR, "tsconfig.json");
 
 const ruleTester = new RuleTester({
   languageOptions: {
