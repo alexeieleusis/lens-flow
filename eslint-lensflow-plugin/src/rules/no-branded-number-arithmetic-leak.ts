82154d9c278bbce298863460c6fe0eda7d7b797b
Alexei Diaz <alexei.eleusis@gmail.com>
2026-06-08 15:42:25 -0700

    fix: address build and test failures

--- eslint-lensflow-plugin/src/rules/no-branded-number-arithmetic-leak.ts
+++ eslint-lensflow-plugin/src/rules/no-branded-number-arithmetic-leak.ts
@@ -10,8 +10,8 @@ const ARITHMETIC_OPS = new Set(["+", "-", "*", "/", "%"]);
 function hasBrandProperty(type: ts.Type): boolean {
   const props = type.getProperties();
   return props.some((p) => {
-    const name = p.escapedName as string;
-    return name === "_brand" || name === "__brand" || /Brand$/.test(name);
+    const name = String(p.escapedName);
+    return name.includes("_brand") || /Brand$/.test(name);
   });
 }
 
