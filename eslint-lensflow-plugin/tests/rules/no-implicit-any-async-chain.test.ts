7f45fa72fbe16a933afbb28454be919d7a2bc199
Alexei Diaz <alexei.eleusis@gmail.com>
2026-06-12 21:36:59 -0700

    feat: add rule no-implicit-any-async-chain

---
+++ eslint-lensflow-plugin/tests/rules/no-implicit-any-async-chain.test.ts
@@ -0,0 +1,26 @@
+import { ruleTester } from "../helpers/rule-tester.js";
+import rule from "../../src/rules/no-implicit-any-async-chain.js";
+
+ruleTester.run("no-implicit-any-async-chain", rule, {
+  valid: [
+    `interface Response { items: { name: string }[] }
+const data: Response = await fetch(url).then(r => r.json());`,
+    `const data: any = await fetch(url).json();`,
+    `const data = await fetch(url).then(r => r.text());`,
+    `const data = await fetch(url);`,
+  ],
+  invalid: [
+    {
+      code: `const data = await fetch(url).then(r => r.json());`,
+      errors: [{ messageId: "implicitAnyAsyncChain" }],
+    },
+    {
+      code: `const result = await fetch(url).json();`,
+      errors: [{ messageId: "implicitAnyAsyncChain" }],
+    },
+    {
+      code: `const config = fetch(url).then(r => r.json());`,
+      errors: [{ messageId: "implicitAnyAsyncChain" }],
+    },
+  ],
+});
