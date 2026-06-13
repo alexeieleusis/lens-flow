5d5f1ca61a99a66764997900166868c7b4cc7cc0
Alexei Diaz <alexei.eleusis@gmail.com>
2026-06-12 21:41:26 -0700

    feat: add rule no-overly-broad-generic-constraints

---
+++ eslint-lensflow-plugin/tests/rules/no-overly-broad-generic-constraints.test.ts
@@ -0,0 +1,46 @@
+import { ruleTester } from "../helpers/rule-tester.js";
+import rule from "../../src/rules/no-overly-broad-generic-constraints.js";
+
+ruleTester.run("no-overly-broad-generic-constraints", rule, {
+  valid: [
+    `interface Box<T extends { id: number }> {
+      value: T;
+    }`,
+    `class IdBox implements Box<{ id: number; name: string }> {
+      value: { id: number; name: string } = { id: 1, name: "test" };
+    }`,
+    `interface Container<T extends string> {
+      item: T;
+    }`,
+    `function wrap<T>(value: T): T {
+      return value;
+    }`,
+    `class Repository<T> {
+      find(): T { throw new Error(); }
+    }`,
+  ],
+  invalid: [
+    {
+      code: `class AnyBox implements Box<any> {
+        value: any = {};
+      }`,
+      errors: [{ messageId: "anyTypeArg" }],
+    },
+    {
+      code: `interface Repo<T extends any> {
+        get(): T;
+      }`,
+      errors: [{ messageId: "anyConstraint" }],
+    },
+    {
+      code: `type Wrapper = Box<any>;`,
+      errors: [{ messageId: "anyTypeArg" }],
+    },
+    {
+      code: `class Multi implements Box<any>, List<any> {
+        value: any = {};
+      }`,
+      errors: [{ messageId: "anyTypeArg" }, { messageId: "anyTypeArg" }],
+    },
+  ],
+});
