b7e8f5cda87e84765344260dc6f862d8a4bb79c4
Alexei Diaz <alexei.eleusis@gmail.com>
2026-06-12 21:36:22 -0700

    feat: add rule no-function-type

---
+++ eslint-lensflow-plugin/tests/rules/no-function-type.test.ts
@@ -0,0 +1,25 @@
+import { ruleTester } from "../helpers/rule-tester.js";
+import rule from "../../src/rules/no-function-type.js";
+
+ruleTester.run("no-function-type", rule, {
+  valid: [
+    `type Fn = (x: number) => string;`,
+    `function register(fn: (event: MouseEvent) => void) { /* ... */ }`,
+    `type Callback = () => void;`,
+    `const handler: (data: string) => boolean = (data) => data.length > 0;`,
+  ],
+  invalid: [
+    {
+      code: `type Fn = Function;`,
+      errors: [{ messageId: "noFunctionType" }],
+    },
+    {
+      code: `function register(fn: Function) { /* ... */ }`,
+      errors: [{ messageId: "noFunctionType" }],
+    },
+    {
+      code: `type Handlers = { click: Function; hover: Function };`,
+      errors: [{ messageId: "noFunctionType" }, { messageId: "noFunctionType" }],
+    },
+  ],
+});
