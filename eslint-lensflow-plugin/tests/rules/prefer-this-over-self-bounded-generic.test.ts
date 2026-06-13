50d9e80ab90cc1e2ab557baafd99204d6e8fef10
Alexei Diaz <alexei.eleusis@gmail.com>
2026-06-12 21:49:01 -0700

    feat: add rule prefer-this-over-self-bounded-generic

---
+++ eslint-lensflow-plugin/tests/rules/prefer-this-over-self-bounded-generic.test.ts
@@ -0,0 +1,45 @@
+import { ruleTester } from "../helpers/rule-tester.js";
+import rule from "../../src/rules/prefer-this-over-self-bounded-generic.js";
+
+ruleTester.run("prefer-this-over-self-bounded-generic", rule, {
+  valid: [
+    `class Builder {
+      name(n: string): this {
+        return this;
+      }
+    }`,
+    `class Foo<T extends string> {
+      value: T;
+      constructor(v: T) { this.value = v; }
+    }`,
+    `class Foo<T> {
+      value: T;
+      constructor(v: T) { this.value = v; }
+    }`,
+    `class Foo<T extends Bar<T>, U> {
+      value: T;
+      other: U;
+    }`,
+    `class Foo<T extends { bar: T }> {
+      value: T;
+    }`,
+  ],
+  invalid: [
+    {
+      code: `class Builder<T extends Builder<T>> {
+        name(n: string): T {
+          return this as any as T;
+        }
+      }`,
+      errors: [{ messageId: "selfBoundedGeneric" }],
+    },
+    {
+      code: `class Chain<T extends Chain<T>> {
+        then(fn: () => void): T {
+          return this as T;
+        }
+      }`,
+      errors: [{ messageId: "selfBoundedGeneric" }],
+    },
+  ],
+});
