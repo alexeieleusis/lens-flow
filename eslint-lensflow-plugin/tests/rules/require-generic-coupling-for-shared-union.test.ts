61d9411cd2283def5e2229cfbc99e05869e152ba
Alexei Diaz <alexei.eleusis@gmail.com>
2026-06-12 21:50:08 -0700

    feat: add rule require-generic-coupling-for-shared-union

---
+++ eslint-lensflow-plugin/tests/rules/require-generic-coupling-for-shared-union.test.ts
@@ -0,0 +1,52 @@
+import { ruleTester } from "../helpers/rule-tester.js";
+import rule from "../../src/rules/require-generic-coupling-for-shared-union.js";
+
+ruleTester.run("require-generic-coupling-for-shared-union", rule, {
+  valid: [
+    `function sum<T extends number | string>(a: T, b: T): T {
+      return a > b ? a : b;
+    }`,
+    `function single(a: number | string): void {
+      console.log(a);
+    }`,
+    `function different(a: number | string, b: boolean | string): void {
+      console.log(a, b);
+    }`,
+    `const fn = <T>(a: T, b: T): T => a;`,
+    `type G = <T>(a: T, b: T) => T;`,
+  ],
+  invalid: [
+    {
+      code: `function sum(a: number | string, b: number | string): number {
+        return Number(a) + Number(b);
+      }`,
+      errors: [{ messageId: "sharedUnionWithoutGeneric" }],
+    },
+    {
+      code: `function concat(a: string | number, b: string | number): string {
+        return String(a) + String(b);
+      }`,
+      errors: [{ messageId: "sharedUnionWithoutGeneric" }],
+    },
+    {
+      code: `const process = (a: number | string, b: number | string) => {
+        return a + b;
+      };`,
+      errors: [{ messageId: "sharedUnionWithoutGeneric" }],
+    },
+    {
+      code: `function triple(a: boolean | number, b: boolean | number, c: boolean | number): void {
+        console.log(a, b, c);
+      }`,
+      errors: [{ messageId: "sharedUnionWithoutGeneric" }],
+    },
+    {
+      code: `type Handler = (a: number | string, b: number | string) => void;`,
+      errors: [{ messageId: "sharedUnionWithoutGeneric" }],
+    },
+    {
+      code: `type F = (a: number | string, b: number | string) => void;`,
+      errors: [{ messageId: "sharedUnionWithoutGeneric" }],
+    },
+  ],
+});
