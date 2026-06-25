a16173e59ba3be04f7fc4041f7d4ee835355979d
Alexei Diaz <alexei.eleusis@gmail.com>
2026-06-25 13:54:33 -0700

    fix: address review comment [on eslint-lensflow-plugin/src/rules/no-typestate-any-bypass.ts]
    - Split imports to use `import type` for TSESTree and TSESLint, keeping AST_NODE_TYPES as a regular import for consistency with sibling rules
    
    Ref: https://github.com/alexeieleusis/lens-flow/pull/299#discussion_r3414276505

--- eslint-lensflow-plugin/src/rules/no-typestate-any-bypass.ts
+++ eslint-lensflow-plugin/src/rules/no-typestate-any-bypass.ts
@@ -1,4 +1,5 @@
-import { AST_NODE_TYPES, TSESTree, TSESLint } from "@typescript-eslint/utils";
+import { AST_NODE_TYPES } from "@typescript-eslint/utils";
+import type { TSESTree, TSESLint } from "@typescript-eslint/utils";
 import { createRule } from "../utils/rule-creator.js";
 
 function isMethodChain(node: TSESTree.Node): boolean {
