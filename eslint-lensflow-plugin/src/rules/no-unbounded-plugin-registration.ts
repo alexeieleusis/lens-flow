4a352591297df46994c084e1e11fb36262a92077
Alexei Diaz <alexei.eleusis@gmail.com>
2026-06-25 14:43:20 -0700

    fix: address review comment [on eslint-lensflow-plugin/src/rules/no-unbounded-plugin-registration.ts]
    - Consolidate TSESTree import from @typescript-eslint/types into @typescript-eslint/utils to reduce dependency surface
    
    Ref: https://github.com/alexeieleusis/lens-flow/pull/299#discussion_r3414329044

--- eslint-lensflow-plugin/src/rules/no-unbounded-plugin-registration.ts
+++ eslint-lensflow-plugin/src/rules/no-unbounded-plugin-registration.ts
@@ -1,6 +1,5 @@
-import type { TSESTree } from "@typescript-eslint/types";
 import { createRule } from "../utils/rule-creator.js";
-import type { TSESLint } from "@typescript-eslint/utils";
+import type { TSESLint, TSESTree } from "@typescript-eslint/utils";
 import { getKeys } from "eslint-visitor-keys";
 
 const DUPLICATE_CHECK_METHODS = new Set([
