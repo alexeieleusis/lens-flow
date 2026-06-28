3ba8f4aa036fb37dd6e27ea0122f4b2279d6ad0f
Alexei Diaz <alexei.eleusis@gmail.com>
2026-06-28 13:10:16 -0700

    fix: address review comment [on tests/rules/no-premature-monad-extraction.test.ts]
    - Add invalid test cases for all 14 untested extraction methods:
      - EXTRACT_METHODS: getOrElse, getOrNull, extract, toOption, toNullable, fromEither, fromOption, fromIO
      - NS_EXTRACT_METHODS: O.getOrElse, O.fold, O.fromOption, E.fromEither, TE.fold, TE.fromIO, W.getOrElse
    
    Ref: https://github.com/alexeieleusis/lens-flow/pull/299#discussion_r3426296481

--- eslint-lensflow-plugin/tests/rules/no-premature-monad-extraction.test.ts
+++ eslint-lensflow-plugin/tests/rules/no-premature-monad-extraction.test.ts
@@ -42,5 +42,146 @@ ruleTester.run("no-premature-monad-extraction", rule, {
       );`,
       errors: [{ messageId: "prematureExtraction" }],
     },
+    // EXTRACT_METHODS: getOrElse
+    {
+      code: `pipe(
+        value,
+        getOrElse(() => defaultValue),
+        String
+      );`,
+      errors: [{ messageId: "prematureExtraction" }],
+    },
+    // EXTRACT_METHODS: getOrNull
+    {
+      code: `pipe(
+        value,
+        getOrNull,
+        String
+      );`,
+      errors: [{ messageId: "prematureExtraction" }],
+    },
+    // EXTRACT_METHODS: extract
+    {
+      code: `pipe(
+        eitherValue,
+        extract,
+        String
+      );`,
+      errors: [{ messageId: "prematureExtraction" }],
+    },
+    // EXTRACT_METHODS: toOption
+    {
+      code: `pipe(
+        eitherValue,
+        toOption,
+        someHandler
+      );`,
+      errors: [{ messageId: "prematureExtraction" }],
+    },
+    // EXTRACT_METHODS: toNullable
+    {
+      code: `pipe(
+        optionValue,
+        toNullable,
+        String
+      );`,
+      errors: [{ messageId: "prematureExtraction" }],
+    },
+    // EXTRACT_METHODS: fromEither
+    {
+      code: `pipe(
+        eitherValue,
+        fromEither,
+        nextStep
+      );`,
+      errors: [{ messageId: "prematureExtraction" }],
+    },
+    // EXTRACT_METHODS: fromOption
+    {
+      code: `pipe(
+        optionValue,
+        fromOption(nullValue),
+        nextStep
+      );`,
+      errors: [{ messageId: "prematureExtraction" }],
+    },
+    // EXTRACT_METHODS: fromIO
+    {
+      code: `pipe(
+        ioValue,
+        fromIO,
+        nextStep
+      );`,
+      errors: [{ messageId: "prematureExtraction" }],
+    },
+    // NS_EXTRACT_METHODS: O.getOrElse
+    {
+      code: `pipe(
+        optionValue,
+        O.getOrElse(() => defaultValue),
+        String
+      );`,
+      errors: [{ messageId: "prematureExtraction" }],
+    },
+    // NS_EXTRACT_METHODS: O.fold
+    {
+      code: `pipe(
+        optionValue,
+        O.fold(
+          () => handleNone(),
+          val => val * 2
+        ),
+        String
+      );`,
+      errors: [{ messageId: "prematureExtraction" }],
+    },
+    // NS_EXTRACT_METHODS: O.fromOption
+    {
+      code: `pipe(
+        optionValue,
+        O.fromOption,
+        nextStep
+      );`,
+      errors: [{ messageId: "prematureExtraction" }],
+    },
+    // NS_EXTRACT_METHODS: E.fromEither
+    {
+      code: `pipe(
+        eitherValue,
+        E.fromEither,
+        nextStep
+      );`,
+      errors: [{ messageId: "prematureExtraction" }],
+    },
+    // NS_EXTRACT_METHODS: TE.fold
+    {
+      code: `pipe(
+        taskEitherValue,
+        TE.fold(
+          err => handleError(err),
+          val => val * 2
+        ),
+        String
+      );`,
+      errors: [{ messageId: "prematureExtraction" }],
+    },
+    // NS_EXTRACT_METHODS: TE.fromIO
+    {
+      code: `pipe(
+        taskIoValue,
+        TE.fromIO,
+        nextStep
+      );`,
+      errors: [{ messageId: "prematureExtraction" }],
+    },
+    // NS_EXTRACT_METHODS: W.getOrElse
+    {
+      code: `pipe(
+        theseValue,
+        W.getOrElse(() => defaultValue),
+        String
+      );`,
+      errors: [{ messageId: "prematureExtraction" }],
+    },
   ],
 });
