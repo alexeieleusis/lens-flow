dedd4b09e81cdf3a92cac3c85d5651c83b901a5a
Alexei Diaz <alexei.eleusis@gmail.com>
2026-07-19 23:51:06 -0700

    fix: restore help URL in no-phantom-types-for-simple-state

--- eslint-lensflow-plugin/src/rules/no-phantom-types-for-simple-state.ts
+++ eslint-lensflow-plugin/src/rules/no-phantom-types-for-simple-state.ts
@@ -1,5 +1,8 @@
 import { AST_NODE_TYPES, TSESLint, TSESTree } from "@typescript-eslint/utils";
 import { createRule } from "../utils/rule-creator.js";
+import { knowledgeUrl } from "../utils/knowledge-url.js";
+
+const URL = knowledgeUrl("usecases/UC01-invalid-states.md");
 
 const PHANTOM_NAMES = new Set([
   "_phantom",
@@ -78,7 +81,7 @@ export default createRule({
     },
     messages: {
       phantomTypeOveruse:
-        "Type '{{name}}<{{params}}>' uses phantom type parameter(s) constrained to literal union(s) for simple state. Use a plain literal union type instead. See: https://raw.githubusercontent.com/jpablo/vibe-types/7891def9e1b66bebd95a393b42f3401eba697cd5/plugin/skills/typescript/usecases/UC01-invalid-states.md",
+        "Type '{{name}}<{{params}}>' uses phantom type parameter(s) constrained to literal union(s) for simple state. Use a plain literal union type instead. See: {{url}}",
     },
     schema: [],
   },
@@ -130,6 +133,7 @@ export default createRule({
         data: {
           name: node.id.name,
           params: getParamNames(phantomParams),
+          url: URL,
         },
       });
     }
