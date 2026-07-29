41a3beee96c8a2805cdec10b85d103f386ef779f
Alexei Diaz <alexei.eleusis@gmail.com>
2026-07-29 12:32:19 -0700

    fix: add knowledge section to no-any-parameter-with-typeguard

--- eslint-lensflow-plugin/src/rules/no-any-parameter-with-typeguard.ts
+++ eslint-lensflow-plugin/src/rules/no-any-parameter-with-typeguard.ts
@@ -3,7 +3,7 @@ import { getChildren } from "../utils/ast-helpers.js";
 import { knowledgeUrl } from "../utils/knowledge-url.js";
 import type { TSESLint, TSESTree } from "@typescript-eslint/utils";
 
-const URL = knowledgeUrl("catalog/T02-union-intersection.md");
+const URL = knowledgeUrl("catalog/T02-union-intersection.md", "Using `any` Instead of Unions");
 
 type ParamNode =
   | TSESTree.Identifier
