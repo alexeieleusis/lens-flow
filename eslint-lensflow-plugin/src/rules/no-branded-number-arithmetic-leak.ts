f28558cd355300f8d320c203d50a6ebfb75a8927
Alexei Diaz <alexei.eleusis@gmail.com>
2026-07-29 13:26:10 -0700

    fix: add knowledge section to no-branded-number-arithmetic-leak

--- eslint-lensflow-plugin/src/rules/no-branded-number-arithmetic-leak.ts
+++ eslint-lensflow-plugin/src/rules/no-branded-number-arithmetic-leak.ts
@@ -3,7 +3,7 @@ import { ESLintUtils, TSESLint } from "@typescript-eslint/utils";
 import { createRule } from "../utils/rule-creator.js";
 import { knowledgeUrl } from "../utils/knowledge-url.js";
 
-const URL = knowledgeUrl("catalog/T03-newtypes-opaque.md");
+const URL = knowledgeUrl("catalog/T03-newtypes-opaque.md", "Arithmetic result is no longer branded");
 
 const ARITHMETIC_OPS = new Set(["+", "-", "*", "/", "%"]);
 
