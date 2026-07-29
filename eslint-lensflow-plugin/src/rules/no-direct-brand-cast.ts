02a01d6f0aa549a99700f995890565094becc660
Alexei Diaz <alexei.eleusis@gmail.com>
2026-07-29 14:50:47 -0700

    fix: add knowledge section to no-direct-brand-cast

--- eslint-lensflow-plugin/src/rules/no-direct-brand-cast.ts
+++ eslint-lensflow-plugin/src/rules/no-direct-brand-cast.ts
@@ -3,7 +3,7 @@ import { ESLintUtils, type TSESTree, TSESLint } from "@typescript-eslint/utils";
 import { createRule } from "../utils/rule-creator.js";
 import { knowledgeUrl } from "../utils/knowledge-url.js";
 
-const URL = knowledgeUrl("catalog/T26-refinement-types.md");
+const URL = knowledgeUrl("catalog/T26-refinement-types.md", "Bypassing the constructor");
 
 const SMART_CONSTRUCTOR_RE = /^(parse[A-Z]|tryParse|mustParse)/;
 
