5a2fa9c35619888d61bbf547bd9b9fbf05145e35
Alexei Diaz <alexei.eleusis@gmail.com>
2026-07-29 21:28:41 -0700

    fix: add knowledge section to no-shallow-readonly-mutable-collection

--- eslint-lensflow-plugin/src/rules/no-shallow-readonly-mutable-collection.ts
+++ eslint-lensflow-plugin/src/rules/no-shallow-readonly-mutable-collection.ts
@@ -2,7 +2,7 @@ import { AST_NODE_TYPES, TSESTree, TSESLint } from "@typescript-eslint/utils";
 import { createRule } from "../utils/rule-creator.js";
 import { knowledgeUrl } from "../utils/knowledge-url.js";
 
-const URL = knowledgeUrl("catalog/T21-encapsulation.md");
+const URL = knowledgeUrl("catalog/T21-encapsulation.md", 'Mutable return from "readonly" getter');
 
 function isMutableCollectionType(node: TSESTree.TypeNode): boolean {
   if (node.type === AST_NODE_TYPES.TSArrayType) {
