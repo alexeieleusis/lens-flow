b7e4720a73b7a55ce25a2d770b771e80095e8dff
Alexei Diaz <alexei.eleusis@gmail.com>
2026-07-29 19:45:44 -0700

    fix: add knowledge section to no-override-this-with-base-type

--- eslint-lensflow-plugin/src/rules/no-override-this-with-base-type.ts
+++ eslint-lensflow-plugin/src/rules/no-override-this-with-base-type.ts
@@ -2,7 +2,7 @@ import { AST_NODE_TYPES, TSESTree, TSESLint } from "@typescript-eslint/utils";
 import { createRule } from "../utils/rule-creator.js";
 import { knowledgeUrl } from "../utils/knowledge-url.js";
 
-const URL = knowledgeUrl("catalog/T33-self-type.md");
+const URL = knowledgeUrl("catalog/T33-self-type.md", "Overriding with incompatible return type");
 
 export default createRule({
   name: "no-override-this-with-base-type",
