f1a549cebccb7ceeadd324f7a9b4c2ee6d2965fe
Alexei Diaz <alexei.eleusis@gmail.com>
2026-07-30 00:20:22 -0700

    fix: add knowledge section to prefer-primitive-method-params

--- eslint-lensflow-plugin/src/rules/prefer-primitive-method-params.ts
+++ eslint-lensflow-plugin/src/rules/prefer-primitive-method-params.ts
@@ -3,7 +3,7 @@ import { createRule } from "../utils/rule-creator.js";
 import { walk } from "../utils/ast-helpers.js";
 import { knowledgeUrl } from "../utils/knowledge-url.js";
 
-const URL = knowledgeUrl("usecases/UC10-encapsulation.md");
+const URL = knowledgeUrl("usecases/UC10-encapsulation.md", "Accepting mutable params");
 
 function analyzeFunction(
   context: Parameters<
